826dc7401d377d3e258c1959f5e8a9dae1d07237
nomaan-07 <nomaan07.dev@gmail.com>
2025-03-19 23:49:54 +0330

    refactor: remove shadcn and add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import Results from './pages/Results';
+import Quiz from './pages/Quiz';
+import AppLayout from './ui/AppLayout';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
