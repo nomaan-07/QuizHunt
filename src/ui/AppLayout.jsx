bad072c0abe8ae090510b042417e285a500c0b74
nomaan-07 <nomaan07.dev@gmail.com>
2025-03-21 16:37:07 +0330

    style: reduce  margin-top of AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 function AppLayout() {
   return (
-    <div className="container my-20 w-full md:w-180">
+    <div className="container mt-4 mb-20 w-full md:w-180">
       <Header />
       <div className="mt-10 w-full space-y-10 rounded-xl bg-slate-50 p-4 md:space-y-14 md:p-7 dark:bg-cyan-900">
         <Outlet />
